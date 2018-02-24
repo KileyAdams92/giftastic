87991106522e94f3e9e5a167c851b7df743641be
Kiley Adams <kileyadams@gmail.com>
2018-02-24 17:39:04 -0600

    on click for play/pause functioning now, and limited items to 10 successfully

--- javascript/app.js
+++ javascript/app.js
@@ -4,14 +4,14 @@ function createButtons() {
   for (i = 0; i < topics.length; i++) {
     makeButton(topics[i]);
   }
+  bindingClicks();
 }
 function makeButton(topic) {
   $(".buttons").append(
-    '<button type="button" class="gifButton btn btn-outline-info">' +
+    '<button type="button" class="gifButton btn btn-secondary">' +
       topic +
       "</button>"
   );
-  bindingClicks();
 }
 createButtons();
 
@@ -45,13 +45,44 @@ function addButtons(buttonText) {
     var character = $(this).attr("data-character");
     var result = response.data;
     console.log(result);
+    //     for (var k = 0; k < result.length; k++) {
+    //       var rating = result[k].rating;
+    //       var p = $("<p>").text("Rating: " + rating);
+    //       var characterImage = $("<img>");
+    //       characterImage.attr("src", result[k].images.fixed_height.url);
+    //       $("#gifs").append(p);
+    //       $("#gifs").append(characterImage);
+    //     }
+    //   });
+    // }
+
+    //trialed this solution - didn't work (gifs only show 6 for button 1, 5 for button 2, 4 for button 3, etc. and gifs stay in still image)
+
     for (var k = 0; k < result.length; k++) {
       var rating = result[k].rating;
       var p = $("<p>").text("Rating: " + rating);
       var characterImage = $("<img>");
-      characterImage.attr("src", result[k].images.fixed_height.url);
       $("#gifs").append(p);
       $("#gifs").append(characterImage);
+      characterImage.attr("src", result[k].images.fixed_height_still.url);
+      characterImage.attr("data-state", "still");
+      characterImage.addClass("gifImage");
+      characterImage.attr("data-animate", result[k].images.fixed_height.url);
+      characterImage.attr(
+        "data-still",
+        result[k].images.fixed_height_still.url
+      );
     }
   });
 }
+
+$(document).on("click", ".gifImage", function() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
